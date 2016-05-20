f38774568e297bc7ae1817749a07eae3a5b94fc3
jiej <njustjiangjie@163.com>
2016-05-20 17:29:59 +0800

    mtime_spider

--- movie_spider/spider_demo.py
+++ movie_spider/spider_demo.py
@@ -9,6 +9,7 @@ import time
 
 from Queue import Queue
 from time import sleep
+from datetime import datetime
 
 from bs4 import BeautifulSoup
 from lxml import etree
@@ -18,38 +19,46 @@ from spider_thread import spider_thread
 from spider_db import ys66_db
 
 class ys66_spider(spider):
-    url_xpath = '//div[@class="listBox"]/ul/li/div[@class="listInfo"]/h3/a/@href'
-    upload_time_xpath = '//div[@class="listBox"]/ul/li/div[@class="listInfo"]/p[3]/text()'
-
     def __init__(self):
         pass
 
     @classmethod
-    def crawler(self, url_queue, file_handle, deadline):
+    def crawler(self, thread_id, url_queue, logger_handle, deadline):
         page_stop_flag = 0
         empty_time = 0
+        sleep_time = 5
         url = ''
         while True:
-            if empty_time > 10 :
+            if empty_time > 100 :
                 break
             try:
                 url = url_queue.get(0)
                 print "\n consumed url from Q size now", url_queue.qsize()
-                print "size now", url_queue.qsize()
+                # print "size now", url_queue.qsize()
             except:
-                sleep(4)
+                sleep(sleep_time)
                 empty_time += 1
                 continue
 
             if re.search('http://www.66ys.tv/\w*?/index\_?\d{0,4}\.html', url) is not None:
 
-                response = requests.get(url)
-                dom = etree.HTML(response.content)
+
+                try:
+                    response = requests.get(url)
+                except:
+                    logger_handle.write(url + ' page request wrong')
+
+                # dom = etree.HTML(response.content)
                 soup = BeautifulSoup(response.content, 'lxml') # making soup
 
-                upload_time_list = dom.xpath(self.upload_time_xpath)
-                url_list = dom.xpath(self.url_xpath)
-                sleep(2)
+                logger_handle.write(url + 'parsing page for upload_time & url_link ...' + '\n')
+
+                upload_time_list = [tag.find_all('p')[2].text for tag in soup.select('div[class="listInfo"]')]
+                # upload_time_list = dom.xpath(self.upload_time_xpath)
+
+                url_list = [tag['href'] for tag in soup.select('div[class="listInfo"] h3 a')]
+                # url_list = dom.xpath(self.url_xpath)
+
                 print len(url_list), len(upload_time_list)
                 if len(url_list) == len(upload_time_list):
                     for time_str, url_str in zip(upload_time_list, url_list):
@@ -60,20 +69,20 @@ class ys66_spider(spider):
                             page_stop_flag = 1
                             break
                         print "producing url for Q..."
+                        # logger_handle.write('producing new movie detail info url ...' + '\n')
                         url_queue.put(url_str, 1)
-                        print "size now", url_queue.qsize()
-                        sleep(0.5)
 
                 if page_stop_flag == 0:
                     # get next page url
                     next_page_tag = soup.find_all('a', text = u'下一页')
                     if len(next_page_tag) > 0:
                         next_page_url = next_page_tag[0]['href']
-                        print "producing url for Q..."
+                        # print "producing url for Q..."
+                        # logger_handle.write('producing new movie list page url ...' + '\n')
                         url_queue.put(next_page_url, 1)
-                        print "size now", url_queue.qsize()
-                        sleep(0.5)
+                        # print "size now", url_queue.qsize()
             else:
+                logger_handle.write('thread ' + thread_id + 'parsing detail page' + url + '\n')
                 response = requests.get(url)
                 soup = BeautifulSoup(response.content, 'lxml') # making soup
 
@@ -83,6 +92,7 @@ class ys66_spider(spider):
                     movie_type = re.findall('http://www.66ys.tv/(.*?)/.*', url)[0]
                     movie_type = movie_type.encode('utf8')
                 except IndexError:
+                    logger_handle.write(url + ' movie_type index out of range '+'\n')
                     print "index out of range"
 
                 # get movie name by xpath
@@ -91,6 +101,7 @@ class ys66_spider(spider):
                 try:
                     movie_name = movie_name_tag[0].text.strip().strip(u'《').strip(u'》').encode('utf8')
                 except IndexError:
+                    logger_handle.write(url + ' movie_name index out of range '+'\n')
                     print "index out of range"
 
                 # get ftp or http download info
@@ -103,14 +114,21 @@ class ys66_spider(spider):
                             continue
                         if u'在线观看' in x:
                             continue
+                        if y.startswith(u'http'):
+                            continue
                         tmp_info = {}
                         tmp_info['download_filename'] = x.encode('utf8')
                         tmp_info['download_link'] = y.encode('utf8')
                         download_info.append(tmp_info)
 
                 # # get wangpan link info
+                try:
+                    utf8_body = unicode(response.content, 'gbk').encode('utf8','ignore')
+                except UnicodeDecodeError:
+                    # print u"编码转换错误"
+                    logger_handle.write('Encode/Decode Error in ' + url + '\n')
+                    continue
                 wangpan_info = []
-                utf8_body = unicode(response.content, 'gbk').encode('utf8','ignore')
                 wangpan_patt = '网盘[\w\W]*?>(.*?)</a> 密码：(.*?)</td>'
                 wangpan_lst = re.findall(wangpan_patt, utf8_body)
                 for pan in wangpan_lst:
@@ -119,34 +137,60 @@ class ys66_spider(spider):
                     tmp_pan_info['wangpan_pwd'] = pan[1]
                     wangpan_info.append(tmp_pan_info)
 
-                record_item = {
-                    'movie_name' : movie_name,
-                    'movie_type' : movie_type,
-                    'download_info' : download_info,
-                    'wangpan_info' : wangpan_info,
-                }
-                ys66_db.insert_one_record(record_item)
+                if len(download_info) > 0 or len(wangpan_info) > 0:
+                    record_item = {
+                        'movie_name' : movie_name,
+                        'movie_type' : movie_type,
+                        'movie_url' : url,
+                        'download_info' : download_info,
+                        'wangpan_info' : wangpan_info,
+                    }
+                    ys66_db.insert_one_record(record_item)
+                else:
+                    logger_handle.write('no download_info in' + url + '\n')
 
 
 
-def main():
-    deadline = time.mktime(time.strptime('2016-05-03', '%Y-%m-%d'))
-    q = Queue(32)
-    q.put('http://www.66ys.tv/xijupian/index.html', 1)
-    q.put('http://www.66ys.tv/aiqingpian/index.html', 1)
+def main(start_url, thread_num, last_crawled_date):
+
+    deadline = time.mktime(time.strptime(last_crawled_date, '%Y-%m-%d'))
+    q = Queue(50)
+    for item in start_url:
+        q.put(item, 1)
+    # q.put('http://www.66ys.tv/xijupian/index.html', 1)
+    # q.put('http://www.66ys.tv/aiqingpian/index.html', 1)
     threads = []
-    num = 5
-    f = open('movie.txt', 'w')
-    for i in range(num):
-        t = spider_thread(ys66_spider.crawler, (q, f, deadline), 't'+str(i+1))
+    logger_handle = open('logger2.txt', 'a')
+    for i in range(thread_num):
+        t = spider_thread(ys66_spider.crawler, ('crawler'+str(i+1), q, logger_handle, deadline), 'crawler'+str(i+1))
         threads.append(t)
 
-    for i in range(num):
+    for i in range(thread_num):
         threads[i].start()
 
-    for i in range(num):
+    for i in range(thread_num):
         threads[i].join()
-    f.close()
+
+    logger_handle.close()
 
 if __name__ == '__main__':
-    main()
+
+    start = datetime.now()
+    start_url = [
+        # 'http://www.66ys.tv/xijupian/index.html',
+        # 'http://www.66ys.tv/aiqingpian/index.html',
+        'http://www.66ys.tv/dongzuopian/index.html',
+        # 'http://www.66ys.tv/kehuanpian/index.html',
+        # 'http://www.66ys.tv/zhanzhengpian/index.html',
+        # 'http://www.66ys.tv/kongbupian/index.html',
+        # 'http://www.66ys.tv/jilupian/index.html',
+        # 'http://www.66ys.tv/bd/index.html',
+        # 'http://www.66ys.tv/3ddy/index.html',
+    ]
+    last_crawled_date = '2000-05-01'
+    thread_num = 5
+    main(start_url, thread_num, last_crawled_date)
+    end = datetime.now()
+
+    print u"耗时：", end - start
+
