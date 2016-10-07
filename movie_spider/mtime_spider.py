5e27d89f85684a9b488b40702a947622b10020e6
jiej <njustjiangjie@163.com>
2016-10-07 14:17:28 +0800

    upgrade

--- movie_spider/mtime_spider.py
+++ movie_spider/mtime_spider.py
@@ -23,8 +23,8 @@ import logging
 logging.basicConfig(level=logging.DEBUG,  
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  
                     datefmt='%a, %d %b %Y %H:%M:%S',  
-                    filename='movie_spider.log',  
-                    filemode='a')
+                    filename='log\\movie_spider.log',  
+                    filemode='w')
 
 def text_filter(ss):
     return ss.replace('·', '-').replace('&#183;', '-')
@@ -37,7 +37,7 @@ def mtime_spider(thread_id, year_queue, least_comment_num):
         'Ajax_CallBackMethod' : 'SearchMovieByCategory',
         'Ajax_CrossDomain' : 1,
         'Ajax_RequestUrl' : 'http://movie.mtime.com/movie/search/section/#',
-        't' : '201693014374394791',
+        't' : '201610411553323886',
         # 'Ajax_CallBackArgument9' : 2000,
         # 'Ajax_CallBackArgument10' : 2000,
         'Ajax_CallBackArgument11' : 0,
@@ -74,6 +74,7 @@ def mtime_spider(thread_id, year_queue, least_comment_num):
         while i < 10001:
             # 如果停止翻页标志位=1， 跳出翻页循环
             if page_stop_flag == 1:
+                logging.info(str(thread_id) + " 评分人数少于 "+str(least_comment_num)+", 不再翻页\n")
                 break
             data['Ajax_CallBackArgument18'] = i;    # 设置页码
             data['Ajax_CallBackArgument9'] = year
@@ -90,8 +91,10 @@ def mtime_spider(thread_id, year_queue, least_comment_num):
             try:
                 response_json = json.loads(content)
             except:
-                logging.warning('载入 '+ str(year) + '年' + str(i) + '页网页内容出错, 停止爬取')
-                break
+                logging.warning('载入 '+ str(year) + '年' + str(i) + '页网页内容出错, 停止爬取\n')
+                i += 1
+                continue
+                # break
 
             # 改用正则表达式匹配
             html = ''
@@ -99,113 +102,170 @@ def mtime_spider(thread_id, year_queue, least_comment_num):
                 html = response_json['value']['listHTML']
             else:
                 if response_json.has_key('value'):
-                    logging.warning('载入 '+ str(year) + '年' + str(i) + '页网页内容出错, 停止爬取')
-                    break
+                    logging.warning('载入 '+ str(year) + '年' + str(i) + '页网页内容出错, 停止爬取\n')
+                    i += 1
+                    continue
+                    # break
 
             lst = re.findall(u'<h3 class=\"normal mt6\"><a.*?href=\"(.*?)\">(.*?)</a>', html)
             if len(lst) == 0:
-                logging.warning('匹配 '+ str(year) + '年' + str(i) + '页电影信息出错, 忽略该页')
+                logging.warning('匹配 '+ str(year) + '年' + str(i) + '页电影信息出错, 忽略该页\n')
                 i += 1
                 continue
 
             movie_url_list, movie_name_list = zip(*lst)
             comment_counts_list = re.findall(u'<p class=\"c_666 mt6\">(\d*?人评分)</p>', html)
             if not len(movie_url_list) == len(movie_name_list) == len(comment_counts_list):
-                logging.warning(str(year) + '年' + str(i) + '页电影信息数量不匹配, 忽略该页')
+                logging.warning(str(year) + '年' + str(i) + '页电影信息数量不匹配, 忽略该页\n')
                 i += 1
                 continue
-                
+            
+            print len(movie_url_list), len(movie_name_list), len(comment_counts_list)
+
             if len(movie_url_list) == len(movie_name_list) == len(comment_counts_list):
                 for index, text in enumerate(comment_counts_list):
                     if page_stop_flag == 1:
-                        logging.info(str(thread_id) + " 评分人数少于 "+str(least_comment_num)+", 不再翻页")
                         break
                     comment_num = int(text.replace(u'人评分', ''))
-                    if comment_num >= least_comment_num:
-                        movie_name = movie_name_list[index].encode('utf8')
-                        detail_url = movie_url_list[index].encode('utf8')
-                        detail_r = requests.get(detail_url)
-
-                        # 剧情信息
-                        plots_url = detail_url + 'plots.html'
-                        plots_html = requests.get(plots_url, headers=headers).content
-                        plots_text = ''
-                        soup = BeautifulSoup(plots_html, 'lxml') # making soup
-                        try:
-                            plots_text = [tag.text for tag in soup.select('div[class="plots_box"] p')][0]
-                        except:
-                            pass
-                        plots_text = plots_text.strip()
-                        
-                        # 导演信息
-                        director_lst = re.findall('<a.*?rel="v:directedBy">(.*?)</a>', detail_r.content)
-                        if len(director_lst) > 0:
-                            director = text_filter(director_lst[0])
-
-                        # 领衔主演信息
-                        starring_lst = re.findall('<a.*?rel="v:starring">(.*?)</a>', detail_r.content)
-                        if len(starring_lst) > 0:
-                            starring = text_filter(starring_lst[0])
-                        
-                        # 主演信息（包括领衔主演）
-                        actor_url = detail_url + 'fullcredits.html'
-                        actor_list = []
-                        html = requests.get(actor_url, headers=headers).content
-                        actor_character_patt = \
-                        '<dd>.*?class=\"actor_tit\".*?<h3>.*?<a.*?>(.*?)</a>.*?class=\"character_tit\">.*?<h3>(.*?)</h3>.*?</dd>'
-                        actor_character_list = re.findall(actor_character_patt, html)
-                        print u"演员表和人物表", len(actor_character_list)
-                        actor_list.extend([(text_filter(x), text_filter(y)) for x, y in actor_character_list[:20]])
-
-                        # 影片评论
-                        comment_list = []
-                        for k in range(1, 11):
-                            comment_url = detail_url + 'reviews/short/new'
-                            if k==1:
-                                comment_url += '.html'
-                            else:
-                                comment_url += '-' + str(k) + '.html'
-                            print comment_url
-                            comment_r = requests.get(comment_url, headers=headers)
-                            comment_html = comment_r.content
-                            comment_patt = \
-                            '<div tweetid=\"\d{1,10}\".*?class=\"mod_short\">.*?<h3>(.*?)</h3>.*?<span class=\"db_point ml6\">(.*?)</span>'
-                            comment_patt_no_score = \
-                            '<div tweetid=\"\d{1,10}\".*?class=\"mod_short\">.*?<h3>(.*?)</h3>.*?<p class=\"mt6 px12 clearfix\">(.*?)</p>'
-                            tmp_list = re.findall(comment_patt_no_score, comment_html)
-                            
-                            for item in tmp_list:
-                                comment_text = item[0]
-                                lst = re.findall('<span class=\"db_point ml6\">(.*?)</span>', item[1])
-                                if len(lst) == 1:
-                                    comment_score = float(lst[0])
-                                else:
-                                    # print u"此用户没有评分"
-                                    comment_score = -1.0
-                                comment_list.append((comment_text, comment_score))
-                            
-                            sleep(1 + random.uniform(0, 2))
-
-                        print thread_id, movie_name_list[index], comment_num, director.decode('utf8'), year
-
-                        record_item = {
-                            'movie_name': movie_name,
-                            'show_year': year,
-                            'comment_counts': comment_num,
-                            'plots_text': plots_text,
-                            'director': director,
-                            'starring': starring,
-                            'url': detail_url,
-                            'comment_list': comment_list,
-                            'actor_list': actor_list,
-                        }
-                        mtime_db.insert_one_record(record_item)
-                    else:
+                    if comment_num < least_comment_num:
                         print thread_id, u"评分人数少于", least_comment_num, u"不再翻页"
                         page_stop_flag = 1
-                    sleep(1 + random.uniform(0, 5))             # 爬完一部电影的全部信息，需要休息一段时间
+                        continue
+                    
+                    movie_name = movie_name_list[index].encode('utf8')
+                    detail_url = movie_url_list[index].encode('utf8')
+                    
+                    movie_id = detail_url.replace('http://movie.mtime.com', '').strip('/')
+                    update_filter = {
+                        'movie_id': movie_id,
+                    }
+                    try:
+                        res = mtime_db.find_one_record(update_filter)
+                        if res is not None:
+                            logging.warning("影片" + movie_id + "已经存在\n" )
+                            print u"该影片已经存在"
+                            continue
+                    except:
+                        logging.warning("查找影片ID" + movie_id + "出错\n" )
+                        print "查找影片ID" + movie_id + "出错\n"
+                        continue
+
+                    
+                    print detail_url
+                    detail_r = requests.get(detail_url, headers=headers).content
+
+                    # 剧情信息
+                    print u"正在获取剧情信息..."
+                    plots_url = detail_url + 'plots.html'
+                    print plots_url
+                    plots_html = requests.get(plots_url, headers=headers).content
+                    plots_text = ''
+                    soup = BeautifulSoup(plots_html, 'lxml') # making soup
+                    try:
+                        plots_text_list = [tag.text for tag in soup.select('div[class="plots_box"] p')]
+                        for item in plots_text_list:
+                            item = item.strip()
+                            if item != '':
+                                plots_text = item
+                                break
+                    except:
+                        pass
+                    
+                    # 导演信息
+                    print u"正在获取导演信息..."
+                    director_lst = re.findall('<a.*?rel="v:directedBy">(.*?)</a>', detail_r)
+                    director = ''
+                    if len(director_lst) > 0:
+                        director = text_filter(director_lst[0])
+
+                    # 领衔主演信息
+                    print u"正在获取领衔主演信息..."
+                    starring = ''
+                    starring_lst = re.findall('<a.*?rel="v:starring">(.*?)</a>', detail_r)
+                    if len(starring_lst) > 0:
+                        starring = text_filter(starring_lst[0])
+                    
+                    # 主演信息（包括领衔主演）
+                    print u"正在获取演职员信息..."
+                    actor_url = detail_url + 'fullcredits.html'
+                    actor_list = []
+                    actor_html = requests.get(actor_url, headers=headers).content
+                    
+                    # actor_character_patt = \
+                    # '''<dd> *?<div class=\"actor_tit\">[\w\W]*?<h3><a.*?>(.*?)</a>[\w\W]*?<div class=\"character_tit\">[\w\W]*?<h3>([\w\W]*?)</h3>[\w\W]*?</dd>'''
+                    # actor_character_list = re.findall(actor_character_patt, actor_html, timeout=3)
+                    # print u"演员表和人物表", len(actor_character_list)
+                    # actor_list += [(text_filter(x), text_filter(y)) for x, y in actor_character_list[:20]]
+
+                    # try:
+                    #     actor_character_list = re.findall(actor_character_patt, actor_html, timeout=3)
+                    #     print u"演员表和人物表", len(actor_character_list)
+                    #     actor_list += [(text_filter(x), text_filter(y)) for x, y in actor_character_list[:20]]
+                    # except:
+                    #     pass
+
+                    soup = BeautifulSoup(actor_html, 'lxml') # making soup
+                    ele_list = soup.select('div[class="db_actor"] dd')
+                    for ele in ele_list[:10]:
+                        inner_ele = ele.findChildren('div', recursive=False)
+                        if len(inner_ele) == 2:
+                            actor_name = (inner_ele[0].select('h3')[0].text).encode('utf8')
+                            character_name = (inner_ele[1].select('h3')[0].text).encode('utf8')
+                            actor_list.append((text_filter(actor_name), text_filter(character_name)))
+
+                    # 影片评论
+                    print u"正在获取影片评论信息..."
+                    comment_list = []
+                    for k in range(1, 11):
+                        comment_url = detail_url + 'reviews/short/new'
+                        if k==1:
+                            comment_url += '.html'
+                        else:
+                            comment_url += '-' + str(k) + '.html'
+                        print comment_url
+                        comment_r = requests.get(comment_url, headers=headers)
+                        comment_html = comment_r.content
+                        comment_patt = \
+                        '<div tweetid=\"\d{1,10}\".*?class=\"mod_short\">.*?<h3>(.*?)</h3>.*?<span class=\"db_point ml6\">(.*?)</span>'
+                        comment_patt_no_score = \
+                        '<div tweetid=\"\d{1,10}\".*?class=\"mod_short\">.*?<h3>(.*?)</h3>.*?<p class=\"mt6 px12 clearfix\">(.*?)</p>'
+                        tmp_list = re.findall(comment_patt_no_score, comment_html)
+                        
+                        for item in tmp_list:
+                            comment_text = item[0]
+                            lst = re.findall('<span class=\"db_point ml6\">(.*?)</span>', item[1])
+                            if len(lst) == 1:
+                                comment_score = float(lst[0])
+                            else:
+                                # print u"此用户没有评分"
+                                comment_score = -1.0
+                            comment_list.append((comment_text, comment_score))
+                        
+                        sleep(3 + random.uniform(0, 1))
+                    
+                    try:
+                        print thread_id, movie_name.decode('utf8'), comment_num, director.decode('utf8'), year
+                    except:
+                        pass
+                    
+                    record_item = {
+                        'movie_id': movie_id,
+                        'movie_name': movie_name,
+                        'show_year': year,
+                        'comment_counts': comment_num,
+                        'plots_text': plots_text,
+                        'director': director,
+                        'starring': starring,
+                        'url': detail_url,
+                        'comment_list': comment_list,
+                        'actor_list': actor_list,
+                    }
+                    mtime_db.insert_one_record(record_item)
+                    # mtime_db.update_one_record(update_filter, {"$set":record_item})
+                    
+                    sleep(2 + random.uniform(0, 5))             # 爬完一部电影的全部信息，需要休息一段时间
             i += 1
-            sleep(6 + random.uniform(0, 5))             # 爬完一页20部电影信息，需要休息足够久
+            sleep(8 + random.uniform(0, 5))             # 爬完一页20部电影信息，需要休息足够久
 
 
 def main(start_year, end_year, thread_num, least_comment_num):
@@ -230,11 +290,11 @@ if __name__ == '__main__':
     
     start = datetime.now()
 
-    start_year = 2016
-    end_year = 2016
+    start_year = 2013
+    end_year = 2014
     mtime_db.collection = mtime_db.db['mtime_movie_'+str(start_year)+'_'+str(end_year)]
     thread_num = 2
-    least_comment_num = 100
+    least_comment_num = 1000
     main(start_year, end_year, thread_num, least_comment_num)
 
     end = datetime.now()
--- movie_spider/spider_db.py
+++ movie_spider/spider_db.py
@@ -26,12 +26,31 @@ class mtime_db(object):
     db = client['movie_home']
     collection = db['mtime_movie_2016']
 
+    
+    @classmethod
+    def find_one_record(self, filter):
+        try:
+            res = self.collection.find_one(filter)
+            return res
+        except:
+            print "##"
+            return None
+
     @classmethod
     def insert_one_record(self, item):
         try:
             self.collection.insert_one(item)
         except:
             print "##"
+    
+    @classmethod
+    def update_one_record(self, filter, item, upsert=True):
+        try:
+            res = self.collection.update_one(filter, item, upsert=upsert)
+            print res.matched_count, res.modified_count
+        except:
+            print "##"
+
 
 class mtime_db_actor(object):
     server = 'localhost'
